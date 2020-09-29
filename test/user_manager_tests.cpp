23c0f9e678f3d3881fa93f7be6001ade0a294f06
Aymeric Pellé <aympelle@gmail.com>
2020-09-29 15:14:38 +0200

    v0.1.0:APPT#7: Add class user_manager.

---
+++ test/user_manager_tests.cpp
@@ -0,0 +1,72 @@
+#include <appt/user_manager.hpp>
+#include <gtest/gtest.h>
+#include <cstdlib>
+
+class ut_user : public appt::user
+{
+public:
+    virtual ~ut_user() = default;
+
+    ut_user(const std::string& name = "") : name(name) {}
+
+    std::string name;
+};
+
+using ut_user_sptr = std::shared_ptr<ut_user>;
+
+TEST(user_manager_tests, test_create_user)
+{
+    appt::user_manager<ut_user> users;
+    ut_user_sptr anonyme_user = users.create_user();
+    ut_user_sptr zeus_user = users.create_user("Zeus");
+    ASSERT_EQ(anonyme_user->id(), 0);
+    ASSERT_EQ(zeus_user->id(), 1);
+    ASSERT_EQ(anonyme_user->name, "");
+    ASSERT_EQ(zeus_user->name, "Zeus");
+}
+
+TEST(user_manager_tests, test_release_user_id)
+{
+    appt::user_manager<ut_user> users;
+    ut_user_sptr anonyme_user = users.create_user();
+    ut_user_sptr zeus_user = users.create_user("Zeus");
+    ut_user::id_type zeus_id = zeus_user->id();
+    zeus_user.reset();
+    users.release_user(zeus_id);
+    ut_user_sptr athena_user = users.create_user("Athena");
+    ut_user::id_type athena_id = athena_user->id();
+    ASSERT_EQ(zeus_id, athena_id);
+}
+
+TEST(user_manager_tests, test_release_user)
+{
+    appt::user_manager<ut_user> users;
+    users.create_user();
+    ut_user_sptr zeus_user = users.create_user("Zeus");
+    ut_user::id_type zeus_id = zeus_user->id();
+    users.release_user(zeus_user);
+    ut_user_sptr athena_user = users.create_user("Athena");
+    ut_user::id_type athena_id = athena_user->id();
+    ASSERT_EQ(zeus_id, athena_id);
+    ASSERT_EQ(athena_user->name, "Athena");
+}
+
+TEST(user_manager_tests, test_release_user_2)
+{
+    appt::user_manager<ut_user> users;
+    users.create_user("Hades");
+    ut_user_sptr zeus_user = users.create_user("Zeus");
+    users.create_user("Heracles");
+    ut_user::id_type zeus_id = zeus_user->id();
+    users.release_user(zeus_user);
+    ut_user_sptr athena_user = users.create_user("Athena");
+    ut_user::id_type athena_id = athena_user->id();
+    ASSERT_EQ(zeus_id, athena_id);
+    ASSERT_EQ(athena_user->name, "Athena");
+}
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
