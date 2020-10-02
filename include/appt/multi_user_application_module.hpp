acae37a545724a3a3ae8532e3ccb54c4b091ace1
Aymeric Pellé <aympelle@gmail.com>
2020-10-02 16:06:21 +0200

    v0.1.0:APPT#13: Rename appt::multi_user_mod appt::mdec::multi_user.
    mdec means module decorator.

--- include/appt/multi_user_application_module.hpp
+++ include/appt/multi_user_application_module.hpp
@@ -9,19 +9,21 @@ inline namespace arba
 {
 namespace appt
 {
+namespace mdec // module_decorator
+{
 
 template <typename user_type, typename user_sptr_hash, typename application_module_base_type>
-class multi_user_mod;
+class multi_user;
 
 template <typename user_type, typename user_sptr_hash, typename application_module_base_type>
-class multi_user_mod : public application_module_base_type
+class multi_user : public application_module_base_type
 {
 public:
     using user_set = appt::user_set<user_type, user_sptr_hash>;
     using application_type = typename application_module_base_type::application_type;
 
-    multi_user_mod() {}
-    explicit multi_user_mod(application_type& app);
+    multi_user() {}
+    explicit multi_user(application_type& app);
 
     inline const user_set& users() const { return users_; }
     inline user_set& users() { return users_; }
@@ -33,28 +35,28 @@ private:
 };
 
 template <typename user_type, typename user_sptr_hash, typename application_module_base_type>
-multi_user_mod<user_type, user_sptr_hash, application_module_base_type>::multi_user_mod(multi_user_mod::application_type& app)
+multi_user<user_type, user_sptr_hash, application_module_base_type>::multi_user(multi_user::application_type& app)
     : application_module_base_type(app)
 {
     users_.set_user_manager(app.usr_manager());
 }
 
 template <typename user_type, typename user_sptr_hash, typename application_module_base_type>
-void multi_user_mod<user_type, user_sptr_hash, application_module_base_type>::set_app(multi_user_mod::application_type &app)
+void multi_user<user_type, user_sptr_hash, application_module_base_type>::set_app(multi_user::application_type &app)
 {
     this->application_module_base_type::set_app(app);
     users_.set_user_manager(app.usr_manager());
 }
 
-//-------------------------------------------------------------------------------------------------
+}
 
 template <typename user_type, typename user_sptr_hash, typename app_type = application>
-class multi_user_application_module : public multi_user_mod<user_type, user_sptr_hash, application_module<app_type>>
+class multi_user_application_module : public mdec::multi_user<user_type, user_sptr_hash, application_module<app_type>>
 {
-    using base_ = multi_user_mod<user_type, user_sptr_hash, application_module<app_type>>;
+    using base_ = mdec::multi_user<user_type, user_sptr_hash, application_module<app_type>>;
 
 public:
-    using base_::multi_user_mod;
+    using base_::multi_user;
 };
 
 }
