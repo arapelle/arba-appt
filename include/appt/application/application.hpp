0a7f7ece20afb2fe489515e96f4ed4b91a71bcbe
Aymeric Pellé <aympelle@gmail.com>
2020-10-05 12:56:55 +0200

    v0.1.0:APPT#15: Add class program, decorator class toolkit. Update application implementation.

--- include/appt/application/application.hpp
+++ include/appt/application/application.hpp
@@ -1,8 +1,7 @@
 #pragma once 
 
-#include "program_args.hpp"
-#include <rsce/resource_manager.hpp>
-#include <evnt/evnt.hpp>
+#include "decorator/toolkit.hpp"
+#include "program.hpp"
 #include <string>
 
 inline namespace arba
@@ -10,28 +9,13 @@ inline namespace arba
 namespace appt
 {
 
-class application
+class application : public adec::toolkit<program>
 {
 public:
     template <typename other_application_type>
     using rebind_t = application;
 
-    application(int argc, char** argv);
-    application(const program_args& args = program_args());
-
-    inline const program_args& args() const { return program_args_; }
-    inline const rsce::resource_manager& resource_manager() const { return resource_manager_; }
-    inline       rsce::resource_manager& resource_manager() { return resource_manager_; }
-    inline const evnt::event_manager& event_manager() const { return event_manager_; }
-    inline       evnt::event_manager& event_manager() { return event_manager_; }
-    inline const vlfs::virtual_filesystem& virtual_filesystem() const { return virtual_filesystem_; }
-    inline       vlfs::virtual_filesystem& virtual_filesystem() { return virtual_filesystem_; }
-
-private:
-    program_args program_args_;
-    rsce::resource_manager resource_manager_;
-    vlfs::virtual_filesystem virtual_filesystem_;
-    evnt::event_manager event_manager_;
+    using toolkit<program>::toolkit;
 };
 
 }
