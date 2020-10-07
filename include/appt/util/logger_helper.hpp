9eb02d1a3d60cc5944a2131feff48b98a2bc9551
Aymeric Pellé <aympelle@gmail.com>
2020-10-07 16:04:26 +0200

    v0.2:APPT#12: Add class logger_helper which has tools to create convenient log sinks.

---
+++ include/appt/util/logger_helper.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <appt/version.hpp>
+#include <spdlog/sinks/sink.h>
+#include <filesystem>
+
+inline namespace arba
+{
+namespace appt
+{
+
+class logger_helper
+{
+public:
+    ~logger_helper() = delete;
+    static std::filesystem::path log_dir(const std::string& name);
+    static spdlog::sink_ptr create_console_sink();
+    static spdlog::sink_ptr create_file_sink(const std::filesystem::path& log_file, std::size_t max_size = 1024*1024*4,
+                                             std::size_t max_files = 128);
+};
+
+}
+}
