b0856c438d03c4e8809b1c6700ed6b3330a7522f
Aymeric Pellé <aympelle@gmail.com>
2023-07-24 13:24:09 +0200

    v0.12.0:#31: Test inline namespace in unit tests.

--- test/application_tests.cpp
+++ test/application_tests.cpp
@@ -11,7 +11,7 @@ char** argv = cs_args.data();
 
 TEST(application_tests, test_constructor_empty)
 {
-    appt::application app;
+    arba::appt::application app;
     ASSERT_TRUE(app.args().empty());
 }
 
