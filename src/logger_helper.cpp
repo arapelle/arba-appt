fda0be228989654edc9ec33c0177045ff181fa75
Aymeric Pellé <aympelle@gmail.com>
2023-10-18 17:41:06 +0200

    v0.13.0: Small changes in log patterns.

--- src/logger_helper.cpp
+++ src/logger_helper.cpp
@@ -16,14 +16,14 @@ std::filesystem::path logger_helper::log_dir(const std::filesystem::path& log_di
 spdlog::sink_ptr logger_helper::create_console_sink()
 {
     std::shared_ptr console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-    console_sink->set_pattern("[%^%8l%$][%Y-%m-%d %H:%M:%S.%f][%n]:  %v");
+    console_sink->set_pattern("[%^%8l%$][%Y-%m-%d %H:%M:%S.%f][%n]: %v");
     return console_sink;
 }
 
 spdlog::sink_ptr logger_helper::create_file_sink(const std::filesystem::path &log_file, std::size_t max_size, std::size_t max_files)
 {
     std::shared_ptr file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(log_file.string(), max_size, max_files);
-    file_sink->set_pattern("[%8l][%Y-%m-%d %H:%M:%S.%f][%n.%t][%@ %!]: %v");
+    file_sink->set_pattern("[%8l][%Y-%m-%d %H:%M:%S.%f][%n %t][%@ %!]: %v");
     return file_sink;
 }
 
