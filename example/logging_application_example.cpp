56a146c7f05571464a648669250c2852fa3b56c4
Aymeric Pellé <aympelle@gmail.com>
2023-10-24 18:04:51 +0200

    v0.13.0:#39: Improve loggin_application_example.

--- example/logging_application_example.cpp
+++ example/logging_application_example.cpp
@@ -2,7 +2,10 @@
 
 #include <arba/appt/application/application.hpp>
 #include <arba/appt/application/decorator/logging.hpp>
+#include <arba/appt/util/logging/logging_macro.hpp>
 #include <spdlog/spdlog.h>
+#include <source_location>
+#include <iostream>
 
 namespace example
 {
@@ -21,8 +24,14 @@ public:
 
     void run()
     {
-        SPDLOG_LOGGER_TRACE(logger(), __PRETTY_FUNCTION__);
+        log_trace_arg<int>();
         SPDLOG_LOGGER_INFO(logger(), "info");
+    }
+
+    template <class Arg>
+    void log_trace_arg()
+    {
+        ARBA_APPT_LOGGER_TRACE(logger());
     }
 };
 
@@ -30,10 +39,21 @@ public:
 
 int main(int argc, char** argv)
 {
-    example::logging_application app(appt::program_args(argc, argv));
-    app.init();
-    app.run();
-    SPDLOG_LOGGER_INFO(app.logger(), "EXIT SUCCESS");
+    std::filesystem::path app_log_path;
+    {
+        example::logging_application app(appt::program_args(argc, argv));
+        app_log_path = app.log_path();
+        app.init();
+        app.run();
+        SPDLOG_LOGGER_INFO(app.logger(), "EXIT SUCCESS");
+    }
+    std::cout << "Application file log:" << std::endl;
+    std::ifstream stream(app_log_path);
+    for (std::string line; stream;)
+    {
+        std::getline(stream, line);
+        std::cout << line << std::endl;
+    }
 
     return EXIT_SUCCESS;
 }
