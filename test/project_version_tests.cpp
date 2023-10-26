ccafb0cfc90a5a5a1cdcc8329a88e7f417875131
Aymeric Pellé <aympelle@gmail.com>
2023-10-26 12:54:45 +0200

    v0.14.0: Start version.

--- test/project_version_tests.cpp
+++ test/project_version_tests.cpp
@@ -6,7 +6,7 @@
 TEST(project_version_tests, test_version_macros)
 {
     const unsigned major = 0;
-    const unsigned minor = 13;
+    const unsigned minor = 14;
     const unsigned patch = 0;
     ASSERT_EQ(ARBA_APPT_VERSION_MAJOR, major);
     ASSERT_EQ(ARBA_APPT_VERSION_MINOR, minor);
