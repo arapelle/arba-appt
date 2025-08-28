5b12a925a8de564770de481f1bb6396319e554cb
Aymeric Pellé <aympelle@gmail.com>
2025-08-28 20:15:45 +0200

    v0.18.0: Starts dev version.

--- test/project_version_tests.cpp
+++ test/project_version_tests.cpp
@@ -5,7 +5,7 @@
 TEST(project_version_tests, test_version_core)
 {
     constexpr unsigned major = 0;
-    constexpr unsigned minor = 17;
+    constexpr unsigned minor = 18;
     constexpr unsigned patch = 0;
     static_assert(arba::appt::version.core() == arba::cppx::numver(major, minor, patch));
 }
